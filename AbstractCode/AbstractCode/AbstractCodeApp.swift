4f2d99bf33bd5109caac1b52f24daba8476bd1d0
TAGonSoft <iosif.moldovan@tagonsoft.ro>
2022-03-01 20:07:52 +0200

    Init Xcode project

---
+++ AbstractCode/AbstractCode/AbstractCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbstractCodeApp.swift
+//  AbstractCode
+//
+//  Created by Iosif Moldovan on 01.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct AbstractCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AbstractCode/AbstractCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AbstractCode
+//
+//  Created by Iosif Moldovan on 01.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
